4dbd80731b6e618322757705dec4807cc687c115
vaibhavd2103 <bountydange@gmail.com>
2024-10-16 20:16:11 +0200

    feat: landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 // import Image from "next/image";
 
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="">
-      <h1>Hello</h1>
+    <div className="container bg-violet-600 w-screen h-screen flex flex-col justify-center items-center">
+      <h1 className="title">Welcome to market place!</h1>
+      <p className="font-semibold">
+        Continue to{" "}
+        <Link href="/login">
+          <button className="btn-secondary">Login/Register</button>
+        </Link>
+      </p>
     </div>
   );
 }
