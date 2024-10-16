f09a34c3a827229ceb4d87d98e76dc8b0a89686b
vaibhavd2103 <bountydange@gmail.com>
2024-10-16 19:16:45 +0200

    patch: hosted backend url update

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://market-place-backend-kb1j.onrender.com/api",
 });
 
 export default instance;
